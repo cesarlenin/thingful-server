444ccba2f3a9973db85138095ec2cf0d45aba524
Cesar Morales <cesarlenin2@live.com>
2020-05-14 09:23:16 -0700

    change ro bcrypt on seeds

--- seeds/seed.thingful_tables.sql
+++ seeds/seed.thingful_tables.sql
@@ -8,12 +8,12 @@ TRUNCATE
 
 INSERT INTO thingful_users (user_name, full_name, nickname, password)
 VALUES
-  ('dunder', 'Dunder Mifflin', null, 'password'),
-  ('b.deboop', 'Bodeep Deboop', 'Bo', 'bo-password'),
-  ('c.bloggs', 'Charlie Bloggs', 'Charlie', 'charlie-password'),
-  ('s.smith', 'Sam Smith', 'Sam', 'sam-password'),
-  ('lexlor', 'Alex Taylor', 'Lex', 'lex-password'),
-  ('wippy', 'Ping Won In', 'Ping', 'ping-password');
+  ('dunder', 'Dunder Mifflin', null, '$2a$12$ulhTRXemH.aOPypChfQBveOdZEX9NDFFEcoEvaWAN0bL9w9rekmkq'),
+  ('b.deboop', 'Bodeep Deboop', 'Bo', '$2a$12$IhQm5OFW83TQI0S4qFikLuI3Xi82xFMZTuegr5D1Uf8uo530UkJre'),
+  ('c.bloggs', 'Charlie Bloggs', 'Charlie', '$2a$12$9Wk6PfleCMBmgfWeWc9Uc.faZcoF31A9MJSi.Ibja9uFUFey/eT.m'),
+  ('s.smith', 'Sam Smith', 'Sam', '$2a$12$MPolTKFaXvbba08zj7LEc.ldLi.BaeNP6MHSoopAqYZBMi2Q8R5bG'),
+  ('lexlor', 'Alex Taylor', 'Lex', '$2a$12$KJm4ChFd92glu/8HDStg8ujeKH5Nx93WikffVwvjzGJ70UL2HYWIu'),
+  ('wippy', 'Ping Won In', 'Ping', '$2a$12$V.ySGrWrLADZk5N2fudmTepPCa8bohFRuXHmNeboblIttP78Kf2Fm');
 
 INSERT INTO thingful_things (title, image, user_id, content)
 VALUES
