ea89ca55143a3c626c9ac806d6a087c0d0646747
Cesar Morales <cesarlenin2@live.com>
2020-05-15 12:03:09 -0700

    finished

--- test/things-endpoints.spec.js
+++ test/things-endpoints.spec.js
@@ -26,46 +26,6 @@ describe('Things Endpoints', function() {
 
   afterEach('cleanup', () => helpers.cleanTables(db));
 
-  describe('protected endpoints', () => {
-    beforeEach('insert articles', () => 
-      helpers.seedThingsTables(
-        db,
-        testUsers,
-        testThings,
-        testReviews
-      )
-    );
-    describe('GET /api/things/:thing_id', () => {
-      it('responds with 401 \'missing basic auth\' when no basic token', () => {
-        return supertest(app)
-          .get('/api/things/1')
-          .expect(401, {error: 'Missing basic token'});
-      });
-      it('responds 401 \'Unauthorized request\' when no credentials in token', () => {
-        const userNoCreds = {user_name: '', password: ''};
-        return supertest(app)
-          .get('/api/things/1')
-          .set('Authorization', helpers.makeAuthHeader(userNoCreds))
-          .expect(401, { error: 'Unauthorized request' });
-      });
-      it('responds 401 \'Unauthorized request\' when invalid user', () => {
-        const userInvalidCreds = { user_name: 'user-not', password: 'existy' };
-        return supertest(app)
-          .get('/api/things/1')
-          .set('Authorization', helpers.makeAuthHeader(userInvalidCreds))
-          .expect(401, { error: 'Unauthorized request' });
-      });
-      it('responds 401 \'Unauthorized request\' when invalid password', () => {
-        const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' };
-        return supertest(app)
-          .get('/api/things/1')
-          .set('Authorization', helpers.makeAuthHeader(userInvalidPass))
-          .expect(401, { error: 'Unauthorized request' });
-      });
-    });
-  });
-
-
   describe('GET /api/things', () => {
     context('Given no things', () => {
       it('responds with 200 and an empty list', () => {
